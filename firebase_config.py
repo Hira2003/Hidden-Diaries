28430acf3eb00ad807ab43623af72b91a20c0e6d
Hira2003 <hirafuyu2003@gmail.com>
2025-06-25 16:50:01 +0100

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,6 +1,8 @@
-import firebase_admin
-from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("firebase_key.json")
+import firebase_admin
+from firebase_admin import credentials
+
+cred = credentials.Certificate("path/to/serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
+
 db = firestore.client()
